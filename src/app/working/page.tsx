f59e7a952e85a9b566797834fe1b547e8b7bbcef
PROSPER ABAYOMI <pablocodes3846@gmail.com>
2026-05-04 11:29:19 +0100

    hopefully...

--- src/app/working/page.tsx
+++ src/app/working/page.tsx
@@ -1,13 +1,26 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Socials from "@/components/Socials";
 import { motion } from "framer-motion";
-
+type Theme = "dark" | "light";
 export default function ManagementPage() {
+    const [theme, setTheme] = useState<Theme>("dark");
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // 1. Theme Persistence Logic
+  useEffect(() => {
+    const savedTheme =
+      (localStorage.getItem("theme") as Theme | null) || "dark";
+    setTheme(savedTheme);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <main className="mx-auto py-8 w-[90%]">
-    <Header />
+          <Header theme={theme} setTheme={setTheme} />
     <div className="flex flex-col h-[70vh] items-center justify-center text-center">
       <motion.div
         initial={{ opacity: 0, y: 40 }}
